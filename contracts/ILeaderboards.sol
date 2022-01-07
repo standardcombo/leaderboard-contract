edbace3cc96f210bda476c61df8d10e497aadef8
standardcombo <standardcombo@gmail.com>
2022-01-06 23:42:26 -0800

    Global leaderboards in Solidity.

---
+++ contracts/ILeaderboards.sol
@@ -0,0 +1,29 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ILeaderboards
+{
+    enum ResetPeriod
+    {
+        Eternal,
+        Yearly,
+        Monthly,
+        Weekly,
+        Daily
+    }
+    function createLeaderboard() external returns(uint256);
+    function getLeaderboard(uint256 leaderboardId) external view returns(string[] memory, uint256[] memory);
+    function clearLeaderboard(uint256 leaderboardId) external;
+    function getResetPeriod(uint256 leaderboardId) external view returns(ResetPeriod);
+    function setResetPeriod(uint256 leaderboardId, ResetPeriod resetPeriod) external;
+    function getCanScoresDecrease(uint256 leaderboardId) external view returns(bool);
+    function setCanScoresDecrease(uint256 leaderboardId, bool canScoresDecrease) external;
+    function getMaxSize(uint256 leaderboardId) external view returns(uint256);
+    function setMaxSize(uint256 leaderboardId, uint256 maxSize) external;
+    function getScore(uint256 leaderboardId, address player) external view returns(uint256);
+    function getPositionForScore(uint256 leaderboardId, uint256 newScore) external view returns(uint256);
+    function submitScore(uint256 leaderboardId, address player, uint256 newScore) external;
+    function registerNickname(string memory nickname) external;
+    function getNickname(address player) external view returns(string memory);
+}
---
+++ contracts/Leaderboards.sol
@@ -0,0 +1,409 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Leaderboards
+{
+    enum ResetPeriod
+    {
+        Eternal,
+        Yearly,
+        Monthly,
+        Weekly,
+        Daily
+    }
+
+    uint8 constant MAX_NICKNAME_LENGTH = 16;
+
+    struct Entry
+    {
+        uint256 score;
+        string nickname;
+    }
+    struct LeaderboardData
+    {
+        ResetPeriod resetPeriod;
+        bool canScoresDecrease;
+        uint256 maxSize;
+        bytes32[] players;
+        uint256[] scores;
+        string[] nicknames;
+    }
+
+    LeaderboardData[] leaderboards;
+    uint256 nextLeaderboardId;
+
+    mapping(uint256 => address) leaderboardOwners;
+    mapping(bytes32 => string) nicknames;
+
+    // A value of 0 means the player does not have a score on that leaderboard
+    mapping(uint256 => mapping(bytes32 => uint256)) playerIndexOneBased;
+
+    /**
+     * 
+     */
+    function createLeaderboard() public returns(uint256)
+    {
+        uint256 id = nextLeaderboardId;
+        nextLeaderboardId++;
+
+        LeaderboardData memory newBoard;
+        newBoard.maxSize = 1000000;
+        leaderboards.push(newBoard);
+
+        leaderboardOwners[id] = msg.sender;
+
+        return id;
+    }
+
+    /**
+     * 
+     */
+    function getLeaderboard(uint256 leaderboardId) public view returns(string[] memory, uint256[] memory)
+    {
+        LeaderboardData memory board = leaderboards[leaderboardId];
+        return (board.nicknames, board.scores);
+    }
+
+    /**
+     * 
+     */
+    function clearLeaderboard(uint256 leaderboardId) public
+    {
+        _checkAuthority(leaderboardId);
+        _clearLeaderboard(leaderboardId);
+    }
+
+    /**
+     * 
+     */
+    function getResetPeriod(uint256 leaderboardId) public view returns(ResetPeriod)
+    {
+        LeaderboardData memory board = leaderboards[leaderboardId];
+        return board.resetPeriod;
+    }
+
+    /**
+     * 
+     */
+    function setResetPeriod(uint256 leaderboardId, ResetPeriod _resetPeriod) public
+    {
+        _checkAuthority(leaderboardId);
+
+        LeaderboardData storage board = leaderboards[leaderboardId];
+        board.resetPeriod = _resetPeriod;
+    }
+
+    /**
+     * 
+     */
+    function getCanScoresDecrease(uint256 leaderboardId) public view returns(bool)
+    {
+        LeaderboardData memory board = leaderboards[leaderboardId];
+        return board.canScoresDecrease;
+    }
+
+    /**
+     * 
+     */
+    function setCanScoresDecrease(uint256 leaderboardId, bool _canScoresDecrease) public
+    {
+        _checkAuthority(leaderboardId);
+
+        LeaderboardData storage board = leaderboards[leaderboardId];
+        board.canScoresDecrease = _canScoresDecrease;
+    }
+
+    /**
+     * 
+     */
+    function getMaxSize(uint256 leaderboardId) public view returns(uint256)
+    {
+        LeaderboardData memory board = leaderboards[leaderboardId];
+        return board.maxSize;
+    }
+
+    /**
+     * 
+     */
+    function setMaxSize(uint256 leaderboardId, uint256 _maxSize) public
+    {
+        _checkAuthority(leaderboardId);
+
+        LeaderboardData storage board = leaderboards[leaderboardId];
+        board.maxSize = _maxSize;
+
+        while (board.scores.length > _maxSize)
+        {
+            playerIndexOneBased[leaderboardId][board.players[board.scores.length - 1]] = 0;
+            board.players.pop();
+            board.scores.pop();
+            board.nicknames.pop();
+        }
+    }
+
+    /**
+     * 
+     */
+    function getScore(uint256 leaderboardId, address player) public view returns(uint256)
+    {
+        bytes32 playerId = _getPlayerId(player);
+        uint256 playerIndex = playerIndexOneBased[leaderboardId][playerId];
+        if (playerIndex > 0)
+        {
+            playerIndex--;
+            LeaderboardData memory board = leaderboards[leaderboardId];
+            if (playerIndex < board.scores.length)
+            {
+                return board.scores[playerIndex];
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 
+     */
+    function getPositionForScore(uint256 leaderboardId, uint256 newScore) public view returns(uint256)
+    {
+        LeaderboardData storage board = leaderboards[leaderboardId];
+
+        for (uint256 i = 0; i < board.scores.length; i++)
+        {
+            if (newScore >= board.scores[i])
+            {
+                return i;
+            }
+        }
+        return board.scores.length;
+    }
+
+    /**
+     * 
+     */
+    function submitScore(uint256 leaderboardId, address player, uint256 newScore) public
+    {
+        _checkAuthority(leaderboardId);
+
+        // TODO : Check reset period and call _clearLeaderboard()
+
+        bytes32 playerId = _getPlayerId(player);
+        LeaderboardData storage board = leaderboards[leaderboardId];
+
+        if (board.maxSize == 0)
+        {
+            return;
+        }
+
+        // The leaderboard is empty, receiving its first score
+        if (board.scores.length == 0)
+        {
+            playerIndexOneBased[leaderboardId][playerId] = 1;
+            board.players.push(playerId);
+            board.scores.push(newScore);
+            board.nicknames.push(_getNickname(playerId));
+
+            return;
+        }
+
+        // Check if this player already has a score on this leaderboard
+        uint256 playerIndex = playerIndexOneBased[leaderboardId][playerId];
+        bool hasPreviousScore = false;
+        if (playerIndex > 0 && playerIndex < board.maxSize)
+        {
+            hasPreviousScore = true;
+            playerIndex--;
+        }
+
+        // Player that is already on this leaderboard
+        if (hasPreviousScore)
+        {
+            // Same score. No change
+            if (newScore == board.scores[playerIndex])
+            {
+                return;
+            }
+            // The new score is better than this player's old score. Search and insert
+            if (newScore > board.scores[playerIndex])
+            {
+                while (playerIndex > 0)
+                {
+                    if (newScore < board.scores[playerIndex - 1])
+                    {
+                        break;
+                    }
+                    // Move other scores down by 1
+                    playerIndexOneBased[leaderboardId][board.players[playerIndex - 1]]++;
+                    board.players[playerIndex] = board.players[playerIndex - 1];
+                    board.scores[playerIndex] = board.scores[playerIndex - 1];
+                    board.nicknames[playerIndex] = board.nicknames[playerIndex - 1];
+
+                    playerIndex--;
+                }
+            }
+            else // The new score is lower than the previous score
+            {
+                // However, the leaderboard may be configured to not allow saving worse scores
+                if ( !board.canScoresDecrease )
+                {
+                    return;
+                }
+                // Search
+                while (playerIndex < board.scores.length - 1)
+                {
+                    uint256 i = playerIndex + 1;
+                    if (newScore >= board.scores[i])
+                    {
+                        break;
+                    }
+
+                    // Move other scores up by 1
+                    playerIndexOneBased[leaderboardId][board.players[i]]--;
+                    board.players[playerIndex] = board.players[i];
+                    board.scores[playerIndex] = board.scores[i];
+                    board.nicknames[playerIndex] = board.nicknames[i];
+
+                    playerIndex++;
+                }
+            }
+        }
+        // New player, with worst score of all
+        else if (newScore < board.scores[board.scores.length - 1])
+        {
+            if (board.scores.length < board.maxSize)
+            {
+                board.players.push(playerId);
+                board.scores.push(newScore);
+                board.nicknames.push(_getNickname(playerId));
+                playerIndexOneBased[leaderboardId][playerId] = board.scores.length;
+            }
+            return;
+        }
+        // New player, inserted in the middle somewhere
+        else
+        {
+            playerIndex = 0;
+            for ( ; playerIndex < board.scores.length; playerIndex++)
+            {
+                // Search for the index to insert at
+                if (newScore >= board.scores[playerIndex])
+                {
+                    // Adjust the score at the bottom of the leaderboard
+                    uint256 i = board.scores.length - 1;
+                    if (board.scores.length < board.maxSize)
+                    {
+                        playerIndexOneBased[leaderboardId][board.players[i]]++;
+                        board.players.push(board.players[i]);
+                        board.scores.push(board.scores[i]);
+                        board.nicknames.push(board.nicknames[i]);
+                    }
+                    else {
+                        playerIndexOneBased[leaderboardId][board.players[i]] = 0;
+                    }
+                    // Move other scores down by 1
+                    while (i > playerIndex)
+                    {
+                        playerIndexOneBased[leaderboardId][board.players[i - 1]]++;
+                        board.players[i] = board.players[i - 1];
+                        board.scores[i] = board.scores[i - 1];
+                        board.nicknames[i] = board.nicknames[i - 1];
+                        i--;
+                    }
+                    break;
+                }
+            }
+        }
+        // Emplace
+        playerIndexOneBased[leaderboardId][playerId] = playerIndex + 1;
+        board.players[playerIndex] = playerId;
+        board.scores[playerIndex] = newScore;
+        board.nicknames[playerIndex] = _getNickname(playerId);
+    }
+
+    /**
+     * 
+     */
+    function registerNickname(string memory _nickname) public
+    {
+        // Limit the size
+        bytes memory strBytes = bytes(_nickname);
+        if (strBytes.length > MAX_NICKNAME_LENGTH)
+        {
+            bytes memory result = new bytes(MAX_NICKNAME_LENGTH);
+            for(uint i = 0; i < MAX_NICKNAME_LENGTH; i++) {
+                result[i] = strBytes[i];
+            }
+            _nickname = string(result);
+        }
+
+        // Save nickname
+        bytes32 playerId = _getPlayerId(msg.sender);
+        _nickname = string(abi.encodePacked(_nickname, " ", _getPlayerIdAbbreviation(playerId)));
+        nicknames[playerId] = _nickname;
+
+        // Update existing entries
+        for (uint256 id = 1; id < nextLeaderboardId; id++)
+        {
+            uint256 playerIndex = playerIndexOneBased[id][playerId];
+            if (playerIndex > 0)
+            {
+                LeaderboardData storage board = leaderboards[id];
+                board.nicknames[playerIndex] = _nickname;
+            }
+        }
+    }
+
+    /**
+     * 
+     */
+    function getNickname(address player) public view returns(string memory)
+    {
+        bytes32 playerId = _getPlayerId(player);
+        return _getNickname(playerId);
+    }
+
+    function _getNickname(bytes32 playerId) internal view returns(string memory)
+    {
+        if (bytes(nicknames[playerId]).length > 0)
+        {
+            return nicknames[playerId];
+        }
+        return _getPlayerIdAbbreviation(playerId);
+    }
+
+    function _getPlayerIdAbbreviation(bytes32 playerId) internal pure returns(string memory)
+    {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory str = new bytes(6);
+        str[0] = "0";
+        str[1] = "x";
+        uint n = 2;
+        for (uint i = 0; n < str.length; i++) {
+            str[n] = alphabet[uint(uint8(playerId[i] >> 4))];
+            str[n+1] = alphabet[uint(uint8(playerId[i] & 0x0f))];
+            n += 2;
+        }
+        return string(str);
+    }
+
+    function _clearLeaderboard(uint256 leaderboardId) internal
+    {
+        LeaderboardData storage board = leaderboards[leaderboardId];
+
+        while (board.scores.length > 0)
+        {
+            board.scores.pop();
+            board.nicknames.pop();
+        }
+    }
+
+    function _checkAuthority(uint256 leaderboardId) internal view
+    {
+        require(leaderboardOwners[leaderboardId] == msg.sender, "No permission to change leaderboard.");
+    }
+
+    function _getPlayerId(address player) internal pure returns(bytes32)
+    {
+        return keccak256(abi.encodePacked(player));
+    }
+}
