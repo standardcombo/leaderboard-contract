82d16848f99d6407c20c2c71286c51d26d047a3c
standardcombo <standardcombo@gmail.com>
2022-01-11 00:57:02 -0800

    Added example game that uses the leaderboard.

---
+++ contracts/ExampleGame.sol
@@ -0,0 +1,32 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ILeaderboard.sol";
+
+contract ExampleGame
+{
+    address leaderboardAddress;
+    uint256 leaderboardId;
+
+    mapping(address => uint256) private points;
+
+    constructor(address _leaderboardAddress)
+    {
+        leaderboardAddress = _leaderboardAddress;
+        leaderboardId = ILeaderboard(leaderboardAddress).createLeaderboard();
+    }
+
+    function earnPoints() public
+    {
+        address player = msg.sender;
+        points[player]++;
+
+        ILeaderboard(leaderboardAddress).submitScore(leaderboardId, player, points[player]);
+    }
+
+    function getLeaderboard() public view returns(string[] memory, uint256[] memory)
+    {
+        return ILeaderboard(leaderboardAddress).getLeaderboard(leaderboardId);
+    }
+}
